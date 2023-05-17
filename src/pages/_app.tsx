c2389c5fcd59be0e4edb37df3f909153607e9023
erailea <toruna@gmail.com>
2023-05-17 14:09:23 +0300

    fix gtm Script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ import '../styles/app.css';
 
 import { Provider } from 'react-redux';
 import themeSlice from '../store/theme-slice';
+import Script from 'next/script';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
@@ -22,17 +23,17 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
         <link rel="icon" href="/favicon-512.png" sizes="512x512" />
         <link rel="apple-touch-icon" href="/favicon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-N4078EZZBM"></script>
-        <script>
+      </Head>
+      <Script
+        id="gtm-script"
+        src="https://www.googletagmanager.com/gtag/js?id=G-N4078EZZBM"
+        strategy="afterInteractive"
+      ></Script>
+      <Script id="gtm-script-runner" strategy="afterInteractive">{`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-
-          gtag('config', 'G-N4078EZZBM');
-        </script>
-      </Head>
-
+          gtag('config', 'G-N4078EZZBM');`}</Script>
       <ErrorBoundary>
         <StrumProvider>
           <Component {...pageProps} />
