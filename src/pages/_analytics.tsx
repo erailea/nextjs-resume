47d4baa5966ae0770b3ca793310a2e9935ac5fd1
erailea <toruna@gmail.com>
2023-05-17 17:09:50 +0300

    track mouse movement

--- src/pages/_analytics.tsx
+++ src/pages/_analytics.tsx
@@ -1,6 +1,23 @@
 import Script from 'next/script';
+import { useEffect } from 'react';
 
 export default function Analytics() {
+  useEffect(() => {
+    function trackMousePosition(event) {
+      const { clientX, clientY } = event;
+      window.gtag('event', 'mousemove', {
+        event_category: 'Mouse',
+        event_label: `X: ${clientX}, Y: ${clientY}`,
+      });
+    }
+
+    document.addEventListener('mousemove', trackMousePosition);
+
+    return () => {
+      document.removeEventListener('mousemove', trackMousePosition);
+    };
+  }, []);
+
   return (
     <>
       <Script
@@ -9,10 +26,16 @@ export default function Analytics() {
         strategy="afterInteractive"
       ></Script>
       <Script id="gtm-script-runner" strategy="afterInteractive">{`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-N4078EZZBM');`}</Script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-N4078EZZBM');`}</Script>
     </>
   );
+}
+
+declare global {
+  interface Window {
+    gtag: any;
+  }
 }
